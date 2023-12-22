ff428721cbe23b842904782a2c2541ff7dc86309
Yuriy Maslenskiy <maslenski2000@mail.ru>
2023-12-22 17:50:43 +0300

    Задание 1

---
+++ hw-2.3.js
@@ -0,0 +1,9 @@
+// Задание 1
+let password = 556372;
+let userPassword = prompt('Введите пароль');
+
+if(userPassword == password){
+    alert('Пароль введен верно')
+}else{
+    alert('Пароль введен неправильно')
+}
