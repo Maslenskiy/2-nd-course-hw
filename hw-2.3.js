ac7987c29a246e1d0a08778fb1c25f7f7cbadd19
Yuriy Maslenskiy <maslenski2000@mail.ru>
2023-12-22 18:55:34 +0300

    Задание 5

--- hw-2.3.js
+++ hw-2.3.js
@@ -1,8 +1,37 @@
-// Задание 4
-let a = '2';
-let b = '3';
-// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
-let sum = Number('2') + Number('3');
-alert(sum);
+// Запрашиваем у пользователя номер месяца
+let monthNumber = prompt("Введите номер месяца (от 1 до 12):");
 
+// Преобразуем введенное значение в число
+monthNumber = parseInt(monthNumber);
 
+// Проверка, что введенный номер месяца находится в допустимом диапазоне
+if (monthNumber < 1 || monthNumber > 12 || isNaN(monthNumber)) {
+    alert("Неверный ввод. Введите число от 1 до 12.");
+} else {
+    // Определение сезона с использованием оператора switch
+    switch (monthNumber) {
+        case 1:
+        case 2:
+        case 12:
+            alert("Зима");
+            break;
+        case 3:
+        case 4:
+        case 5:
+            alert("Весна");
+            break;
+        case 6:
+        case 7:
+        case 8:
+            alert("Лето");
+            break;
+        case 9:
+        case 10:
+        case 11:
+            alert("Осень");
+            break;
+        default:
+            alert("Что-то пошло не так. Попробуйте ещё раз.");
+            break;
+    }
+}
