07deedcafcf94178c1876d172fdc2d67fd7ffc06
Yuriy Maslenskiy <maslenski2000@mail.ru>
2023-12-22 18:07:00 +0300

    Задание 3

--- hw-2.3.js
+++ hw-2.3.js
@@ -1,10 +1,11 @@
-// Задание 2
- let c = 10;
+// Задание 3
+let d = 300;
+let e = 10;
 
- if(c > 0 && c < 10 ){
+if( d > 100 || e > 100){
     console.log('Верно')
- }else{
+}else{
     console.log('Неверно')
- }
+}
 
 
