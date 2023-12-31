3ad56f218b3451ce949bef54c382a629dc924901
Yuriy Maslenskiy <maslenski2000@mail.ru>
2023-12-31 10:20:41 +0300

    Задание 2

--- hw-2.5.js
+++ hw-2.5.js
@@ -8,3 +8,19 @@ function min(a, b) {
     }
   }
 console.log(min(8,4));
+
+
+// Задача 2
+
+function checkEvenOdd(number) {
+  if (number % 2 === 0) {
+      return 'Число четное';
+  } else {
+      return 'Число нечетное';
+  }
+}
+
+// Пример использования функции
+let num = prompt('Введите число: ');
+let result = checkEvenOdd(parseInt(num));
+console.log(result);
