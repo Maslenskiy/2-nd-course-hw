6d480a7be8d435f1fc57f2ccc89b2dffc80d3899
Yuriy <maslenski2000@mail.log>
2025-01-12 17:08:16 +0300

    // Задание 5 // Напишите программу, которая определяет по номеру месяца в году, к какому сезону этот месяц принадлежит. // Например, 1-й месяц (он же январь) принадлежит к сезону «зима». // Для написания программы используйте оператор switch. Для обозначения номера месяца используйте переменную monthNumber, // которая равна 12. Пропишите условие, при котором программа не будет выполняться (номер месяца больше 13).

--- homework-2/hw-2.1.js
+++ homework-2/hw-2.1.js
@@ -1,6 +1,6 @@
 "use strict"
 
-Задание 1
+// Задание 1
 
 const password = '2025';
 const getUserPassword = prompt('Введите пароль');
@@ -36,3 +36,56 @@ let a = '2';
 let b = '3';
 // // Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
 alert(Number(a) + Number(b));
+
+// Задание 5
+// Напишите программу, которая определяет по номеру месяца в году, к какому сезону этот месяц принадлежит. 
+// Например, 1-й месяц (он же январь) принадлежит к сезону «зима».
+// Для написания программы используйте оператор switch. Для обозначения номера месяца используйте переменную monthNumber, 
+// которая равна 12. Пропишите условие, при котором программа не будет выполняться (номер месяца больше 13).
+
+let monthNumber = 13 ;
+
+switch(monthNumber){
+    case 12:
+        console.log(`${monthNumber} - месяц принадлежит к сезону Зима`);
+    break;
+    case 1:
+        console.log(`${monthNumber} - месяц принадлежит к сезону Зима`);
+    break;
+    case 2 :
+        console.log(`${monthNumber} - месяц принадлежит к сезону Зима`);
+    break;
+
+    case 3:
+        console.log(`${monthNumber} - месяц принадлежит к сезону Весна`);
+    break;
+    case 4:
+        console.log(`${monthNumber} - месяц принадлежит к сезону Весна`);
+    break;
+    case 5 :
+        console.log(`${monthNumber} - месяц принадлежит к сезону Весна`);
+    break;
+
+    case 6:
+        console.log(`${monthNumber} - месяц принадлежит к сезону Лето`);
+    break;
+    case 7:
+        console.log(`${monthNumber} - месяц принадлежит к сезону Лето`);
+    break;
+    case 8 :
+        console.log(`${monthNumber} - месяц принадлежит к сезону Лето`);
+    break;
+    
+    case 9:
+        console.log(`${monthNumber} - месяц принадлежит к сезону Осень`);
+    break;
+    case 10:
+        console.log(`${monthNumber} - месяц принадлежит к сезону Осень`);
+    break;
+    case 11 :
+        console.log(`${monthNumber} - месяц принадлежит к сезону Осень`);
+    break;
+
+    default:
+        console.log('Такого номера месяца не существует');
+}
