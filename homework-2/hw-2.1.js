d03d4e0ac2ba12c663d16b813fa1f99063050bd2
Yuriy <maslenski2000@mail.log>
2025-01-12 16:34:26 +0300

    Задание2 Создайте переменную с и запишите в нее любое число . Если с больше 0 и меньше 10 , то выведите в консоль «Верно», иначе выведите «Неверно». Проверьте работу скрипта при значении переменной c, равной 0,10,−3,2 Для решения задачи используйте логический оператор && (логическое И).

--- homework-2/hw-2.1.js
+++ homework-2/hw-2.1.js
@@ -1,6 +1,6 @@
 "use strict"
 
-// Задание 1
+Задание 1
 
 const password = '2025';
 const getUserPassword = prompt('Введите пароль');
@@ -9,4 +9,12 @@ if(getUserPassword === password){
     alert('Пароль введен верно');
 }else{
     alert('Пароль введен неверно');
+}
+
+// Задание 2
+const c = 2;
+if(c > 0 && c < 10){
+    console.log('верно');
+}else{
+    console.log('неверно')
 }
