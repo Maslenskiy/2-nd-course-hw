a66bf447e527449523f0a2d402c370851735bf3d
Yuriy <maslenski2000@mail.log>
2025-01-17 22:32:51 +0300

    Проверка числа на четность или нечетность

--- homework-4/main.js
+++ homework-4/main.js
@@ -1,11 +1,23 @@
 // Задание 1
 
-function smallerOfTwoNumbers (a, b){
-    if(a < b || a === b){
-        return a
-    }else{
-        return b
-    }
+// function smallerOfTwoNumbers (a, b){
+//     if(a < b || a === b){
+//         return a
+//     }else{
+//         return b
+//     }
 
+// }
+// console.log(smallerOfTwoNumbers(8,8))
+
+// Задание 2
+
+function evenOrOdd(num){
+    if( num % 2 === 0 ) {
+        console.log('Число четное')
+    }else{
+        console.log('Число нечетное')
+    }
 }
-console.log(smallerOfTwoNumbers(8,8))
+
+evenOrOdd(9)
