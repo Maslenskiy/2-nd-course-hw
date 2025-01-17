f4ef3a6b3dd6b613170b0e6579a8b29836cfb36b
Yuriy <maslenski2000@mail.log>
2025-01-17 22:37:50 +0300

    функция квадрат числа

--- homework-4/main.js
+++ homework-4/main.js
@@ -10,14 +10,29 @@
 // }
 // console.log(smallerOfTwoNumbers(8,8))
 
-// Задание 2
+//  Задание 2
 
-function evenOrOdd(num){
-    if( num % 2 === 0 ) {
-        console.log('Число четное')
-    }else{
-        console.log('Число нечетное')
-    }
+// function evenOrOdd(num){
+//     if( num % 2 === 0 ) {
+//         console.log('Число четное')
+//     }else{
+//         console.log('Число нечетное')
+//     }
+// }
+
+// evenOrOdd(9)
+
+
+// Задание 3
+
+function squareOfNumber(num){
+    return num ** 2;
 }
 
-evenOrOdd(9)
+const res1 = squareOfNumber(5);
+
+console.log(`Квадрат равен ${res1}`)
+
+const res2 = squareOfNumber(3);
+
+console.log(`Квадрат равен ${res2}`)
