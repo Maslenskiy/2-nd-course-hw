71d9ca179256e72ab9b04fb3c61d1dab7d880405
Yuriy <maslenski2000@mail.log>
2025-01-25 22:43:57 +0300

    Найдите индекс значения 4  в этом массиве.

--- homework-5/main.js
+++ homework-5/main.js
@@ -9,3 +9,9 @@ for(let i = 0; i < numbers.length; i++){
     };
     console.log(numbers[i])
 }
+
+
+//Задание 2
+const numbers2 = [1, 5, 4, 10, 0, 3];
+const indexEl = numbers2.indexOf(4);
+console.log(`Индекс значения 4 в этом массиве равен: ${indexEl}`)
