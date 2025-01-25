220135bc5d034c4c58e289af91480e9951be28fe
Yuriy <maslenski2000@mail.log>
2025-01-25 22:46:14 +0300

    С помощью метода join  выведите элементы массива через пробел (пустую строку ' ' ).

--- homework-5/main.js
+++ homework-5/main.js
@@ -14,4 +14,11 @@ for(let i = 0; i < numbers.length; i++){
 //Задание 2
 const numbers2 = [1, 5, 4, 10, 0, 3];
 const indexEl = numbers2.indexOf(4);
-console.log(`Индекс значения 4 в этом массиве равен: ${indexEl}`)
+console.log(`Индекс значения 4 в этом массиве равен: ${indexEl}`);
+
+//Задание 3
+
+const numbers3 = [1, 3, 5, 10, 20];
+
+const joinNum = numbers3.join(' ');
+console.log(joinNum)
